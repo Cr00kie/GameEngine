689c5304f5f64166e7d83e7bffc9e18504928f52
Adrian Isasi <aisasi505@gmail.com>
2024-11-19 22:15:00 +0100

    Add project files.

---
+++ Components.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public abstract class Component
+    {
+        public GameObject gameObject;
+        public Component(GameObject gameObject) { this.gameObject = gameObject; }
+        public virtual void UpdateComponent()
+        {
+
+        }
+    }
+    public class Transform : Component
+    {
+        public float X0 {  get; private set; }
+        float x;
+        public float X
+        {
+            get { return x; }
+            set { if (value >= 0) { X0 = X; x = value; } }
+        }
+        public float Y0 { get; private set; }
+        float y;
+        public float Y
+        {
+            get { return y; }
+            set { if (value >= 0) { Y0 = Y; y = value; } }
+        }
+        public int width;
+        public int height;
+        public float vx;
+        public float vy;
+
+        public Transform(GameObject gameObject, float x = 0, float y = 0, int width = 0, int height = 0, float vx = 0, float vy = 0) : base(gameObject)
+        {
+            X = x;
+            Y = y;
+            X0 = X;
+            Y0 = Y;
+            this.width = width;
+            this.height = height;
+            this.vx = vx;
+            this.vy = vy;
+        }
+
+        public override void UpdateComponent()
+        {
+            X += vx * GameManager.delta/1000f;
+            Y += vy * GameManager.delta/1000f;
+        }
+    }
+
+    public class SquareCollider : Component
+    {
+        public SquareCollider(GameObject gameObject) : base(gameObject)
+        {
+        }
+
+        public override void UpdateComponent()
+        {
+            bool collisionDetected = false; //for testing
+            List<GameObject> objectsToCheck = new List<GameObject>();
+            objectsToCheck.AddRange(GameManager.CurrScene.gameObjects);
+            foreach(GameObject go in objectsToCheck)
+            {
+                if (CollisionDetector.AABCollision(go, gameObject))
+                {
+                    gameObject.CallCollisionEvent(go);
+                    go.CallCollisionEvent(gameObject);
+                    collisionDetected = true;
+                }
+            }
+            if (collisionDetected)
+            {
+                Console.WriteLine("Collision detected");
+            }
+            else
+            {
+                Console.WriteLine("                  ");
+            }
+        }
+    }
+    public class SpriteRenderer : Component
+    {
+        char[,] sprite;
+        public SpriteRenderer(GameObject gameObject, char[,] sprite) : base(gameObject) { this.sprite = sprite; }
+        public SpriteRenderer(GameObject gameObject, char texture) : base(gameObject)
+        {
+            sprite = new char[gameObject.transform.height,gameObject.transform.width];
+            for(int i = 0; i < sprite.GetLength(0); i++) for(int j = 0; j < sprite.GetLength(1); j++) sprite[i,j] = texture;
+        }
+
+        public override void UpdateComponent()
+        {
+            Renderer.AddObjectToScreen((int)gameObject.transform.X, (int)gameObject.transform.Y, sprite);
+        }
+
+    }
+    public class PlayerMovement : Component
+    {
+        public PlayerMovement(GameObject gameObject) : base(gameObject) { }
+
+        public override void UpdateComponent()
+        {
+            char? input = Input.GetInput();
+
+            switch(input)
+            {
+                case 'a':
+                    gameObject.transform.X--;
+                    break;
+                case 'd':
+                    gameObject.transform.X++;
+                    break;
+                case 'w':
+                    gameObject.transform.Y--;
+                    break;
+                case 's':
+                    gameObject.transform.Y++;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+    public class CollisionManager : Component
+    {
+        public CollisionManager(GameObject gameObject) : base(gameObject)
+        {
+            gameObject.OnCollision += WallCollision;
+            gameObject.OnCollision += SceneBorderCollision;
+        }
+
+        public void WallCollision(GameObject wall)
+        {
+            if (wall.tag == "wall")
+            {
+                float desplazamientoX = 0;
+                float desplazamientoY = 0; 
+                //No es muy bonito pero ahora no se me ocurre nada mejor
+                if (gameObject.transform.X0 - gameObject.transform.X > 0) desplazamientoX = 1;
+                else if (gameObject.transform.X0 - gameObject.transform.X < 0) desplazamientoX = -1;
+                if (gameObject.transform.Y0 - gameObject.transform.Y > 0) desplazamientoY = 1;
+                else if (gameObject.transform.Y0 - gameObject.transform.Y < 0) desplazamientoY = -1;
+                do
+                {
+                    gameObject.transform.X += desplazamientoX;
+                    gameObject.transform.Y += desplazamientoY;
+                } while (CollisionDetector.AABCollision(gameObject, wall));
+            }
+        }
+        public void SceneBorderCollision(GameObject sceneBorder)
+        {
+            switch(sceneBorder.tag)
+            {
+                case "sceneBorderNorth": if(GameManager.ChangeSceneByDirection(1)) gameObject.transform.Y = GameManager.CurrScene.sceneHeight - 2; break;
+                case "sceneBorderSouth": if(GameManager.ChangeSceneByDirection(2)) gameObject.transform.Y = 1;  break;
+                case "sceneBorderWest": if(GameManager.ChangeSceneByDirection(3)) gameObject.transform.X = GameManager.CurrScene.sceneWidth - 2; break;
+                case "sceneBorderEast": if(GameManager.ChangeSceneByDirection(4)) gameObject.transform.X = 1; break;
+                default: break;
+            }
+        }
+    }
+}
---
+++ GameEngine.cs
@@ -0,0 +1,318 @@
+﻿namespace GameEngine
+{
+    public static class GameManager
+    {
+        public static int delta = 25;
+        public static World world = new World(10, 10, 32, 32);
+        public static Scene CurrScene { get; private set; } = world.scenes[0, 0];
+        static int curSceneX;
+        public static int CurSceneX
+        {
+            get
+            {
+                return curSceneX;
+            }
+            set
+            {
+                curSceneX = value;
+                CurrScene = world.scenes[curSceneY, curSceneX];
+            }
+        }
+        static int curSceneY;
+        public static int CurSceneY
+        {
+            get
+            {
+                return curSceneY;
+            }
+            set
+            {
+                curSceneY = value;
+                CurrScene = world.scenes[curSceneY, curSceneX];
+            }
+        }
+
+        public static void ChangeSceneByCoords(int sceneX, int sceneY)
+        {
+            GameObject? player = CurrScene.FindGameObjectByTag("player");//To move the player to the new screen
+            int prevSceneX = CurSceneX;
+            int prevSceneY = CurSceneY;
+            
+            if (sceneX >= 0 && sceneY >= 0 && sceneX < world.scenes.GetLength(1) && sceneY < world.scenes.GetLength(0))
+            {
+                CurSceneX = sceneX;
+                CurSceneY = sceneY;
+                if (player != null)
+                {
+                    CurrScene.InstantiateObject(player); //To move the player to the new screen if the scene has changed
+                    world.scenes[prevSceneY,prevSceneX].DestroyObject(player); //Delete the player from the previous scene
+                }
+            }
+            
+        }
+        public static bool ChangeSceneByDirection(int dir)
+        {
+            ///<summary>1 = up, 2 = down, 3 = left, 4 = right</summary>
+            ///<returns> true if could change screen, false if not </returns>
+            bool couldChangeScene = true;
+            GameObject? player = CurrScene.FindGameObjectByTag("player");//To move the player to the new screen
+            int prevSceneX = CurSceneX;
+            int prevSceneY = CurSceneY;
+
+            switch (dir)
+            {
+                case 1: if (CurSceneY > 0) CurSceneY--; else couldChangeScene = false; break;
+                case 2: if (CurSceneY < world.scenes.GetLength(0)) CurSceneY++; else couldChangeScene = false; break;
+                case 3: if(CurSceneX > 0) CurSceneX--; else couldChangeScene = false; break;
+                case 4: if(CurSceneX < world.scenes.GetLength(1)) CurSceneX++; else couldChangeScene = false; break;
+            }
+            if (couldChangeScene && player != null)
+            {
+                CurrScene.InstantiateObject(player); //To move the player to the new screen if scene has changed
+                world.scenes[prevSceneY, prevSceneX].DestroyObject(player); //Delete the player from the previous scene
+            }
+            return couldChangeScene;
+        }
+    }
+
+    public class GameEngine
+    {
+        public GameEngine(int dt = 100, int worldWidth = 10, int worldHeight = 10,int screenWidth = 32, int screenHeight = 32)
+        {
+            GameManager.delta = dt;
+            GameManager.world = new World(worldWidth, worldHeight, screenWidth, screenHeight);
+        }
+
+        public void RunGameEngine(int startSceneX, int startSceneY)
+        {
+            GameManager.ChangeSceneByCoords(startSceneX, startSceneY); 
+            bool gameRunning = true;
+
+            int frameCount = 0;
+            while (gameRunning)
+            {
+                Input.UpdateInput();
+                GameManager.CurrScene.UpdateObjects();
+                Renderer.RenderScene();
+                Thread.Sleep(GameManager.delta);
+                Console.Title = "frames: " + frameCount + " scenePos: X" + GameManager.CurSceneX + " Y" + GameManager.CurSceneY;
+                frameCount++;
+            }
+        }
+    }
+
+    public static class CollisionDetector
+    {
+        public static bool AABCollision(GameObject gameObject1, GameObject gameObject2)
+        {
+            int x1L = (int)gameObject1.transform.X;
+            int x1R = (int)gameObject1.transform.X + gameObject1.transform.width;
+            int x2L = (int)gameObject2.transform.X;
+            int x2R = (int)gameObject2.transform.X + gameObject2.transform.width;
+
+            int y1U = (int)gameObject1.transform.Y;
+            int y1D = (int)gameObject1.transform.Y + gameObject1.transform.height;
+            int y2U = (int)gameObject2.transform.Y;
+            int y2D = (int)gameObject2.transform.Y + gameObject2.transform.height;
+
+            return (((x1L < x2R && x1L >= x2L)
+               || (x2L < x1R && x2L >= x1L))
+               &&
+               ((y1U < y2D && y1U >= y2U)
+               || (y2U < y1D && y2U >= y1U))
+               && !gameObject2.Equals(gameObject1)
+              );
+        }
+    }
+
+    public static class Input
+    {
+        static char? input;
+        public static void UpdateInput()
+        {
+            if (Console.KeyAvailable)
+            {
+                input = Console.ReadKey(true).KeyChar;
+                while (Console.KeyAvailable) Console.ReadKey(true);
+            }
+            else
+            {
+                input = null;
+            }
+        }
+        public static char? GetInput()
+        {
+            return input;
+        }
+    }
+
+    public static class Renderer
+    {
+        public static char[,] screen = new char[32,32];
+        public static void AddObjectToScreen(int posX, int posY, char[,] sprite)
+        {
+            for (int i = 0; i<sprite.GetLength(0) && i+posY < screen.GetLength(0); i++)
+            {
+                for (int j = 0; j<sprite.GetLength(1) && j + posX < screen.GetLength(1); j++)
+                {
+                    screen[i + posY, j + posX] = sprite[i, j];
+                }
+            }
+        }
+        public static void RenderScene()
+        {
+            //Write the screen
+            Console.CursorVisible = false;
+            string buffer = "";
+            for (int i = 0; i < screen.GetLength(0); i++)
+            {
+                for (int j = 0; j < screen.GetLength(1); j++)
+                {
+                    buffer += screen[i, j];
+                }
+                buffer += "\n";
+            }
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine(buffer);
+            Console.SetCursorPosition(0, 0);
+
+            //Clear the screen
+            for (int i = 0; i < screen.GetLength(0); i++)
+            {
+                for (int j = 0; j < screen.GetLength(1); j++)
+                {
+                    screen[i, j] = ' ';
+                }
+            }
+            Console.SetCursorPosition(GameManager.CurrScene.sceneWidth + 1, 0);
+        }
+    }
+    public class World
+    {
+        public Scene[,] scenes;
+        public World(int wWidth, int wHeight, int sWidth, int sHeight)
+        {
+            scenes = new Scene[wWidth, wHeight];
+            //Fill the world
+            for(int i = 0; i < scenes.GetLength(0); i++)
+            {
+                for(int j = 0; j < scenes.GetLength(1); j++)
+                {
+                    scenes[i,j] = new Scene(sWidth, sHeight);
+                }
+            }
+        }
+        public World(Scene[,] scenes)
+        {
+            this.scenes = scenes;
+        }
+        public void ChangeScene(Scene scene, int posX, int posY)
+        {
+            scenes[posY, posX] = scene;
+        }
+    }
+    public class Scene
+    {
+        public int sceneWidth, sceneHeight;
+        public Scene(int sWidth, int sHeight)
+        {
+            sceneWidth = sWidth;
+            sceneHeight = sHeight;
+        }
+        public List<GameObject> gameObjects = new List<GameObject>();
+
+        public void InstantiateObject(GameObject gameObject, int posX, int posY)
+        {
+            gameObjects.Add(gameObject);
+            gameObject.transform.X = posX;
+            gameObject.transform.Y = posY;
+        }
+        public void InstantiateObject(GameObject gameObject)
+        {
+            gameObjects.Add(gameObject);
+        }
+
+        public void DestroyObject(GameObject gameObject)
+        {
+            gameObjects.Remove(gameObject);
+        }
+
+        public GameObject? FindGameObjectByTag(string tag)
+        {
+            return gameObjects.Find(x => x.tag == tag);
+        }
+
+        public void UpdateObjects()
+        {
+            List<GameObject> objects = new List<GameObject>();
+            objects.AddRange(gameObjects);
+            foreach (GameObject gameObject in objects)
+            {
+                gameObject.Update();
+            }
+        }
+    }
+
+    public class GameObject
+    {
+        List<Component> components = new List<Component>();
+
+        public Transform transform;
+
+        public event Action<GameObject>? OnCollision;
+
+        public string tag;
+
+        public void CallCollisionEvent(GameObject obj)
+        {
+            if (OnCollision != null) OnCollision(obj);
+        }
+
+        public void AddComponent(Component componentToAdd)
+        {
+            if(!components.Contains(componentToAdd)) components.Add(componentToAdd);
+        }
+        public void RemoveComponent(Component componentToRemove)
+        {
+            components.Remove(componentToRemove);
+        }
+        public Component? GetComponent<T>()
+        {
+            return components.Find(x => x.GetType() == typeof(T));
+        }
+        public Component? GetComponent(Type type)
+        {
+            return components.Find(x => x.GetType() == type);
+        }
+
+        public GameObject(string tag, int x = 0, int y = 0, int width = 1, int height = 1)
+        {
+            transform = new Transform(this, x, y, width, height);
+            components.Add(transform);
+            this.tag = tag;
+        }
+        public GameObject(string tag, int x = 0, int y = 0, int width = 1, int height = 1, params Component[] components)
+        {
+            transform = new Transform(this);
+            this.components.Add(transform);
+            foreach (Component comp in components)
+            {
+                AddComponent(comp);
+            }
+            this.tag = tag;
+        }
+
+        public virtual void Update()
+        {
+            foreach (Component component in components)
+            {
+                component.UpdateComponent();
+            }
+        }
+
+        public void Destroy()
+        {
+            GameManager.CurrScene.DestroyObject(this);
+        }
+    }
+}
